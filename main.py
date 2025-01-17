c5b00a68a4cbf43df401a4f57ca2508662be181c
HYOCC <96851545+HYOCC@users.noreply.github.com>
2025-01-17 11:53:51 -0500

    Updated court, increase to 1x2 from 1x1

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import resources.ballObj as ballClass
 import resources.player as playerClass
 
-player1 = playerClass.player('Oscar', 100)
-player2 = playerClass.player('John', 90)
+hitter1 = playerClass.player('Oscar', 30)
+setter1 = playerClass.player('Nick', 80)
+
+hitter2 = playerClass.player('John', 40)
+setter2 = playerClass.player('Johnny', 50)
+
 ball = ballClass.ball()
 
 court1 = [[],
@@ -10,14 +14,14 @@ court1 = [[],
 court2 = [[],
           []]
 
-team1 ={player1: 'OH'}
+team1 ={hitter1: 'OH', setter1: 'S'}
 for key, value in team1.items():
     if value == 'OH':
         court1[1].append(key) 
     elif value == 'S':
         court1[0].append(key)
 
-team2 = {player2: 'OH'}
+team2 = {hitter2: 'OH', setter2: 'S'}
 for key, value in team2.items():
     if value == 'OH':
         court2[1].append(key)
