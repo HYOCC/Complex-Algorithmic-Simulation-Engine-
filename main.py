4991c6aa513a68da405c2c3de8c841fdaaa93b4f
HYOCC <96851545+HYOCC@users.noreply.github.com>
2025-01-17 11:51:51 -0500

    Updated player representation object

--- main.py
+++ main.py
@@ -5,16 +5,26 @@ player1 = playerClass.player('Oscar', 100)
 player2 = playerClass.player('John', 90)
 ball = ballClass.ball()
 
-players = [player1, player2]
+court1 = [[],
+          []]
+court2 = [[],
+          []]
+
+team1 ={player1: 'OH'}
+for key, value in team1.items():
+    if value == 'OH':
+        court1[1].append(key) 
+    elif value == 'S':
+        court1[0].append(key)
+
+team2 = {player2: 'OH'}
+for key, value in team2.items():
+    if value == 'OH':
+        court2[1].append(key)
+    elif value == 'S':
+        court2[0].append(key)
 
 if __name__ == '__main__':
-    while True:
-        for player in players:
-            if ball.state == 'none' or ball.state == 'hit':
-                input(f'{player} hit?')
-                player.hit(ball) 
-                ball.state = 'receive'
-            elif ball.state == 'receive':
-                input(f'{player} receive?')
-                player.receive(ball)
-                ball.state = 'hit'
+    print(court1)
+    print(court2)
+        
--- resources/player.py
+++ resources/player.py
@@ -66,5 +66,9 @@ class player():
     
     def receive(self, ball:ball):
         return calculateRoll(ball, 'receive', self.accuracy)
+    
     def __str__(self):
         return self.name
+
+    def __repr__(self):
+        return self.__str__()
