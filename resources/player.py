f071705a4a6c36280681849b044f0a9c58740af0
HYOCC <96851545+OscarYPSU@users.noreply.github.com>
2025-01-21 21:13:04 -0500

    Made logs easier to read

--- resources/player.py
+++ resources/player.py
@@ -8,15 +8,19 @@ class player():
         self.ballControl = ballControl # currently its the how much speed you can control a spike from receive. ** FUTURE NOTE, will also be player's efficeicny of using a float, adding spins etc
         
     def serve(self, ball, spot:int):
+        print(f'\nCurrent player: {self.name}\n')
         return calcRollServe(ball, self.accuracy, self.strength, spot)
     
     def receive(self, ball, spot:int):
+        print(f'\n\nCurrent player: {self.name}\n')
         return calcRollReceive(ball, self.accuracy, self.ballControl, spot)
     
     def set(self, ball, spot:int):
+        print(f'\n\nCurrent player: {self.name}\n')
         return calcRollSet(ball, self.accuracy, spot)
     
     def spike(self, ball, spot:int):
+        print(f'\n\nCurrent player: {self.name}\n')
         return calcRollSpike(ball, self.accuracy, self.strength, spot)
     
     def __str__(self):
