5571bac43602715e09b90b0374863c360ce93f41
HYOCC <96851545+OscarYPSU@users.noreply.github.com>
2025-01-27 15:06:41 -0500

    implemented player moving
    added a hash map to keep track of player position with O(1)

--- resources/court.py
+++ resources/court.py
@@ -1,21 +1,35 @@
+import resources.player as player
 from resources.turnStructure import queue
 
 class court():
     def __init__(self, players:dict):# players: {playerObj:position} 
         self.court = [[],[],[],
                       [],[],[]]
+        self.hmap = {}# keeps track of the positions of the player directly {player:position}
         
         # puts the player of the team onto the court
         for key, value in players.items():
             if value == 'OH':
                 self.court[0].append(key) 
+                self.hmap[key] = 0
             elif value == 'S':
                 self.court[2].append(key)
+                self.hmap[key] = 2
             elif value == 'L':
                 self.court[4].append(key)
+                self.hmap[key] = 4 
         
         self.queue = queue(players)
+    
+    # moving a player on the court
+    def movePlayer(self, player:player, spot:int):
+        self.court[self.hmap[player]].pop()
+        self.court[spot-1].append(player)
+        self.hmap[player] = spot
+        self.printCourtState()
         
+        return True# work in progress to check player stat if it actually is a successful movement
+    
     # printing out the court to terminal for better visualization
     def printCourtState(self):
         for i in range(len(self.court)):
